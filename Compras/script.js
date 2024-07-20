e4fee83b9bdcaf5abc146e727084a42393c10d33
pepe <alex11ullmann@gmail.com>
2024-07-20 14:55:43 -0300

    modifique tarjetas de compra

--- Compras/script.js
+++ Compras/script.js
@@ -1,13 +1,71 @@
-const productos = ["Papel higienico", "hamburguesas", "patitas", "desodorante en aerosol", "papitas snack", "cerveza miller", "cepillo de dientes", "pan de hamburguesas", "gaseosa de pomelo", "coca cola", "desodorante en barra", "fernet branca"];
-const precios = [1090.00, 2790.00, 5490.00, 1990.00, 2900.00, 1200.00, 745.50, 1535.00, 2900.00, 3100.00, 5820.50, 9900.95];
+"use strict";
 
-var total = document.getElementById("total");
+const productos = ["Papel higienico Simple Hoja x4 un.", "Hamburguesas Paty de carne x4 un.", "Bocaditos de pollo x 800g", "Desodorante Axe x 150ml - Canela y Ambar", "Papas Mc Cain x 700g", "Lata cerveza Miller x1 un.", "Cepillo de Dientes Colgate x1 un.", "Pan Hamburguesas Fargo x4 un.", "Gaseosa Pomelo Schweppes Zero x2,25 lt", "Gaseosa Coca Cola x2,25 lt", "Barra desodorante Old Spice x50 g", "Fernet Branca x750 ml"];
+const precios = [1090, 2790, 5490, 1990, 2900, 1200, 745, 1535, 2900, 3100, 5820, 9900];
+const urlimagenes = ["../ZImag/ph.webp", "../ZImag/hamb.webp", "../ZImag/patitas.webp", "../ZImag/aerosol.webp", "../ZImag/papas.webp", "../ZImag/lata cer.webp", "../ZImag/cepillo dientes.webp", "../ZImag/pan hambur.webp", "../ZImag/pomelo.webp", "../ZImag/coca.webp", "../ZImag/barra.webp", "../ZImag/fernet.webp"];
+const textoalter = ["papel higienico", "hamburguesas", "patitas de pollo", "desodorante en aerosol", "papas snack", "lata de cerveza", "cepillo de dientes", "pan de hamburguesa", "gaseosa pomelo", "coca cola", "aerosol en barra", "fernet"];
+
+//Aca creo la tarjeta
+let tarjetagrande = document.getElementById ("tarjetaconimagen");
+
+function creartarjetagrande () {
+    for (let i = 0; i < urlimagenes.length; i++) {
+    tarjetagrande.innerHTML += `<div>
+    <img src="${urlimagenes[i]}" alt="${textoalter[i]}"> 
+    <div class="botones">
+    <p>${productos[i]}</p>
+    <button type="button" id="botonresta${[i]}">-</button>
+    <input type="text" id="total${[i]}" value="0">
+    <button type="button" id="botonsuma${[i]}" >+</button>
+    <p> $ ${precios[i]}</p>
+    <p>Total</p>
+    <p>$ <input type="text" id="preciototal${[i]}" value="0"></p>
+    </div>
+    </div>`;
+}
+}
+
+creartarjetagrande();
+
+
+//funcion para que el boton me sume
+
+var total = document.getElementById ("total");
+let nuevototal = total.value;
+
+var botonsuma = document.getElementById ("botonsuma");
+botonsuma.addEventListener ("click", sumar1);
+
+function sumar1 () {
+    nuevototal = String(Number(nuevototal) + 1);
+    total.value = nuevototal;
+    multiplicar();
+}
+
+//funcion para que el boton me reste
+
 var botonresta = document.getElementById ("botonresta");
-botonresta.addEventListener("click", restar1);
-
+botonresta.addEventListener ("click", restar1);
 
 function restar1() {
-    total = total+1;
-    total.innerHTML=total;
+    if (nuevototal> 0) {
+        nuevototal = String(Number(nuevototal) - 1);
+        total.value = nuevototal;
+    }
+    else
+        total.value = nuevototal;
+    multiplicar();
 }
+
+//aca va el precio total de cada producto
+
+let preciototal = document.getElementById ("preciototal");
+let preciototalnuevo = preciototal.value;
+
+function multiplicar (){
+    preciototalnuevo = String (Number(nuevototal)) * precios[0];
+    preciototal.value = preciototalnuevo;
+}
+
+//aca va el resultado de la suma de todos los productos
 
--- Contacto/script.js
+++ Contacto/script.js
@@ -1,19 +1,23 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("node:fs");
 
 var nombre = document.getElementById("nombre");
 var correo = document.getElementById("correo");
 var telefono = document.getElementById("telefono");
 var mensaje = document.getElementById("mensaje");
 var telefonostring = telefono.toString();
-var datosusuario = new Array(nombre, correo, telefonostring, mensaje);
+var datosusuario = [];
 
 let boton = document.getElementById ("boton");
 boton.addEventListener("click", guardardatos);
 
-
 function guardardatos() {
-    fs.writeFileSync("./datosclientes.txt", datosusuario);
+    datosusuario [0] = nombre.value;
+    datosusuario [1] = " " + correo.value;
+    datosusuario [2] = " " + telefono.value;
+    datosusuario [3] = " " + mensaje.value;
+
+    let blob = new Blob([datosusuario], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "Datosdeusuario.txt");
     alert("Enviado con exito");
+
 }
