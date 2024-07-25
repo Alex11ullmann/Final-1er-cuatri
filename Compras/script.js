125a171539244002207d612de8953492a242991c
pepe <alex11ullmann@gmail.com>
2024-07-24 23:45:47 -0300

    solucionado la suma y resta de botones

--- Compras/script.js
+++ Compras/script.js
@@ -1,71 +1,42 @@
 "use strict";
 
-const productos = ["Papel higienico Simple Hoja x4 un.", "Hamburguesas Paty de carne x4 un.", "Bocaditos de pollo x 800g", "Desodorante Axe x 150ml - Canela y Ambar", "Papas Mc Cain x 700g", "Lata cerveza Miller x1 un.", "Cepillo de Dientes Colgate x1 un.", "Pan Hamburguesas Fargo x4 un.", "Gaseosa Pomelo Schweppes Zero x2,25 lt", "Gaseosa Coca Cola x2,25 lt", "Barra desodorante Old Spice x50 g", "Fernet Branca x750 ml"];
-const precios = [1090, 2790, 5490, 1990, 2900, 1200, 745, 1535, 2900, 3100, 5820, 9900];
-const urlimagenes = ["../ZImag/ph.webp", "../ZImag/hamb.webp", "../ZImag/patitas.webp", "../ZImag/aerosol.webp", "../ZImag/papas.webp", "../ZImag/lata cer.webp", "../ZImag/cepillo dientes.webp", "../ZImag/pan hambur.webp", "../ZImag/pomelo.webp", "../ZImag/coca.webp", "../ZImag/barra.webp", "../ZImag/fernet.webp"];
-const textoalter = ["papel higienico", "hamburguesas", "patitas de pollo", "desodorante en aerosol", "papas snack", "lata de cerveza", "cepillo de dientes", "pan de hamburguesa", "gaseosa pomelo", "coca cola", "aerosol en barra", "fernet"];
+let productos = ["Papel higienico Simple Hoja x4 un.", "Hamburguesas Paty de carne x4 un.", "Bocaditos de pollo x 800g", "Desodorante Axe x 150ml - Canela y Ambar", "Papas Mc Cain x 700g", "Lata cerveza Miller x1 un.", "Cepillo de Dientes Colgate x1 un.", "Pan Hamburguesas Fargo x4 un.", "Gaseosa Pomelo Schweppes Zero x2,25 lt", "Gaseosa Coca Cola x2,25 lt", "Barra desodorante Old Spice x50 g", "Fernet Branca x750 ml"];
+let precios = [1090, 2790, 5490, 1990, 2900, 1200, 745, 1535, 2900, 3100, 5820, 9900];
+let urlimagenes = ["../ZImag/ph.webp", "../ZImag/hamb.webp", "../ZImag/patitas.webp", "../ZImag/aerosol.webp", "../ZImag/papas.webp", "../ZImag/lata cer.webp", "../ZImag/cepillo dientes.webp", "../ZImag/pan hambur.webp", "../ZImag/pomelo.webp", "../ZImag/coca.webp", "../ZImag/barra.webp", "../ZImag/fernet.webp"];
+let textoalter = ["papel higienico", "hamburguesas", "patitas de pollo", "desodorante en aerosol", "papas snack", "lata de cerveza", "cepillo de dientes", "pan de hamburguesa", "gaseosa pomelo", "coca cola", "aerosol en barra", "fernet"];
+let stock = [20, 15, 5, 25, 40, 25, 20, 7, 18, 13, 9, 3];
+let tarjetagrande = document.querySelector(".tarjetaconimagen");
 
-//Aca creo la tarjeta
-let tarjetagrande = document.getElementById ("tarjetaconimagen");
 
-function creartarjetagrande () {
+function creartarjetagrande() {
     for (let i = 0; i < urlimagenes.length; i++) {
-    tarjetagrande.innerHTML += `<div>
-    <img src="${urlimagenes[i]}" alt="${textoalter[i]}"> 
-    <div class="botones">
-    <p>${productos[i]}</p>
-    <button type="button" id="botonresta${[i]}">-</button>
-    <input type="text" id="total${[i]}" value="0">
-    <button type="button" id="botonsuma${[i]}" >+</button>
-    <p> $ ${precios[i]}</p>
-    <p>Total</p>
-    <p>$ <input type="text" id="preciototal${[i]}" value="0"></p>
-    </div>
-    </div>`;
+        tarjetagrande.innerHTML +=
+            `<div>
+            <img src="${urlimagenes[i]}" alt="${textoalter[i]}"> 
+            <div class="botones">
+                <p>${productos[i]}</p>
+                <input type="number" class="cantAComprar" value="0" min="0" max="${stock[i]}">
+                <p>${precios[i]}</p>
+                <p>Stock total = ${stock[i]}</p>
+            </div>
+        </div>`;
+    }
 }
-}
-
 creartarjetagrande();
 
 
-//funcion para que el boton me sume
+let cantAComprar = document.querySelectorAll(".cantAComprar");
+let botonigual = document.querySelector(".botonIgual");
+let TotalAPagar = document.querySelector(".TotalAPagar");
 
-var total = document.getElementById ("total");
-let nuevototal = total.value;
+botonigual.addEventListener ("click", (e)=>{
+    let sumatotal =0;    
+    cantAComprar.forEach(cantidad => {
+        let pPrecio = cantidad.nextElementSibling.innerHTML;
+        console.log (pPrecio);
+        sumatotal += cantidad.value * pPrecio;
+        console.log (cantidad);
+        TotalAPagar.innerHTML = sumatotal;
+    }); 
 
-var botonsuma = document.getElementById ("botonsuma");
-botonsuma.addEventListener ("click", sumar1);
-
-function sumar1 () {
-    nuevototal = String(Number(nuevototal) + 1);
-    total.value = nuevototal;
-    multiplicar();
-}
-
-//funcion para que el boton me reste
-
-var botonresta = document.getElementById ("botonresta");
-botonresta.addEventListener ("click", restar1);
-
-function restar1() {
-    if (nuevototal> 0) {
-        nuevototal = String(Number(nuevototal) - 1);
-        total.value = nuevototal;
-    }
-    else
-        total.value = nuevototal;
-    multiplicar();
-}
-
-//aca va el precio total de cada producto
-
-let preciototal = document.getElementById ("preciototal");
-let preciototalnuevo = preciototal.value;
-
-function multiplicar (){
-    preciototalnuevo = String (Number(nuevototal)) * precios[0];
-    preciototal.value = preciototalnuevo;
-}
-
-//aca va el resultado de la suma de todos los productos
-
+});
